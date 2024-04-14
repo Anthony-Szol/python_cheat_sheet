21d8da39164e95337c869ca18406995450a973a4
Anthony-Szol <szolanthony@gmail.com>
2024-04-13 21:23:18 -0700

    Operator

---
+++ 3_operators/1_operators.py
@@ -0,0 +1,28 @@
+# Express is a operand (x and y) with an operator (+,-,*,/)
+
+# Arithmetic Operators
+# These are used for arithmetic operators
+# Addition
+a = 1
+b = 2
+print(a + b)
+
+# Subtraction
+c = 3
+d = 1
+print(c-d)
+
+# Multiply
+e = 10
+f = 2
+print(e*f)
+
+# Divide will always yield float value
+g = 5
+h = 5
+print(g/h)
+
+# You can add strings as well (String concatenation)
+first = 'Anthony'
+last = 'Szol'
+print(first + last)
