5bff7afe870ceef580e40d5c9b1cfed367da8f2f
Anthony-Szol <szolanthony@gmail.com>
2024-04-14 21:42:01 -0700

    Print

--- 2_print_function/2_print_challenges.py
+++ 2_print_function/2_print_challenges.py
@@ -1,9 +1,9 @@
 # Time to test your skills and win rewards!
 
-# Question 1: print Hello Anthony
+# Problem 1: print Hello Anthony
 
-# Quesiton 2: print Hello Anthony are you excited? with two print lines
+# Problem 2: print Hello Anthony are you excited? with two print lines
 
-# Question 3: join and print two variables of your choice (Hint: use the + operator)
+# Problem 3: join and print two variables of your choice (Hint: use the + operator)
 
 
--- 2_print_function/3_print_challenge_answers.py
+++ 2_print_function/3_print_challenge_answers.py
@@ -1,13 +1,13 @@
 # Time to test your skills and win rewards!
 
-# Question 1: print Hello Anthony
+# Problem 1: print Hello Anthony
 print('Hello', 'Anthony')
 
-# Quesiton 2: print Hello Anthony are you excited? with two print lines
+# Problem 2: print Hello Anthony are you excited? with two print lines
 print('Hello','Anthony', end=' ')
 print('are you excited?')
 
-# Question 3: join and print two variables of your choice (Hint: use the + operator)
+# Problem 3: join and print two variables of your choice (Hint: use the + operator)
 var1='Iron'
 var2='Man'
 var3= var1 + var2
