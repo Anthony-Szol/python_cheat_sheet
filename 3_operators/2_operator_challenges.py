cebda98d0f5c25f0c0993d0ac6c3c2c1b0bdf89c
Anthony-Szol <szolanthony@gmail.com>
2024-04-14 21:40:31 -0700

    Operator

---
+++ 3_operators/2_operator_challenges.py
@@ -0,0 +1,27 @@
+# Operator Challenge
+
+# Problem 1: 
+# Write a Python function that takes two numbers as input 
+# and returns their sum, difference, product, and quotient 
+#rounded to two decimal places.
+
+# Variables
+a = 4
+b = 6
+
+# Problem 2:
+# Find the remainder of variable c and d
+c = 30
+d = 19
+
+# Problem 3:
+# Make the result true by comparing the following variables
+e = 7
+f = 3
+
+# Problem 4: 
+# Given the initial value, write a sequence of operations 
+# using the assignment operators
+g = 10
+
+
---
+++ 3_operators/3_operator_challenge_answers.py
@@ -0,0 +1,52 @@
+# Operator Challenge Answers
+
+# Problem 1: 
+# Write a Python function that takes two numbers as input 
+# and returns their sum, difference, product.
+
+# Variables
+a = 4
+b = 6
+
+# Sum
+print(a + b)
+
+# Difference
+print(a-b)
+
+# Product
+print(a*b)
+
+# Problem 2:
+# Find the remainder of variable c and d
+c = 30
+d = 19
+print(c%d)
+
+# Problem 3:
+# Make the result true by comparing the following variables
+e = 7
+f = 3
+
+print(e > f)
+print(e >= f)
+print(e != f)
+
+# Problem 4: 
+# Given the initial value, write a sequence of operations 
+# using the assignment operators
+g = 10
+
+g += 15
+print('After g += 15 =', g) 
+# Increase g by 15, g is now 25
+
+g *= 3   
+print('After g *= 3 =', g)
+# Multiply g by 3, g is now 75
+
+g -= 2
+print('After g -= 2 =', g)
+# Reduce g by 2, g is now 73
+
+
