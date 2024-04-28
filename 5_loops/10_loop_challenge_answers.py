2883110679501f9f6692af5043c71b3bfdfcbf08
Anthony-Szol <szolanthony@gmail.com>
2024-04-28 09:44:37 -0700

    Loops

---
+++ 5_loops/10_loop_challenge_answers.py
@@ -0,0 +1,12 @@
+# Print the every 3rd number between 1-10
+i = 1
+while i <= 10:
+    if i % 3 == 0:
+        print(i, end=" ")
+
+    i += 1
+
+# List out the times tables for 12 using 1-10
+n = int(input())
+for m in range(1,10):
+    print(i*n)
---
+++ 5_loops/9_loop_challenges.py
@@ -0,0 +1,7 @@
+# Print the every 3rd number between 1-10
+i = 1
+
+
+# List out the times tables for 12 using 1-10
+m = int(input())
+
