5c8c6303aca3ba3427b41a976a07057dc9c05c99
Anthony-Szol <szolanthony@gmail.com>
2024-05-01 14:38:07 -0700

    OOP

---
+++ L_OOP/1_intro.py
@@ -0,0 +1,21 @@
+# OOPs
+# Object Oriented Programming
+# The principles of OOPs are based on 4 pillars apart from Class and Objects
+# A class has attributes and behaviour
+
+# Abstraction
+# Means displaying only essential information and hiding the details
+# Example: Car will accelerate but internal working of engine is not exposed
+
+# Encapsulation
+# Bundling of data into a single unit
+# Bundling of all methods that can act on an object of that data
+# Car can hav colors, brake, engineand a lot of other methods
+
+# Inheritance
+# When a class derives from another class
+# Any new car company can inherit all the information from car class
+
+# Polymorhism
+# Means having many forms
+# Same class methods can work differently for different objects
---
+++ L_OOP/2_creating_classes.py
@@ -0,0 +1,16 @@
+# It is a blue print to create objects
+# The class keyword is used to create classes
+# By general convention we start name with capital letter
+# Example: int, float, string, etc
+class Car:
+    pass
+
+
+
+# Objects
+# Objects are instances or enitities of a class
+# It has all the properties of it's class
+# eg. 1,2 are objects of int class
+
+honda = Car()
+type(honda)
